79d3d885a48c05aa5a942987fda12f454324e084
Ernie3 <ernestgwilsoniii@gmail.com>
2025-01-22 02:18:07 -0500

    add current photon camera offset (copied from last year)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,6 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -37,7 +36,7 @@ public final class Constants {
       Units.inchesToMeters(-11.7));
 
   /** https://docs.photonvision.org/en/latest/docs/programming/photonlib/robot-pose-estimator.html#creating-a-photonposeestimator */
-  public static final Transform3d kRobotToPhotonCamera = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+  public static final Transform3d kRobotToPhotonCamera = new Transform3d(Units.inchesToMeters(7.6882), Units.inchesToMeters(13.0), Units.inchesToMeters(12.1545), new Rotation3d(0, Math.toRadians(-60), 0));
   public static final String kPhotonCameraName = "4342_AprilTag_1";
 
   // individual offsets after calibrating each module
