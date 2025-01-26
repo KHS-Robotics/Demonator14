b96a9df62646f598fe4f20c6729466664140ebb2
Andrea-dot-sketch <pizzawithnothing@gmail.com>
2025-01-26 13:25:03 -0500

    Servo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,9 +3,9 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.util.PathPlannerLogging;
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.auto.NamedCommands;
+// import com.pathplanner.lib.util.PathPlannerLogging;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 
@@ -26,6 +26,7 @@ import frc.robot.commands.drive.DriveSwerveWithXbox;
 import frc.robot.subsystems.cameras.DemonLimelightCamera;
 import frc.robot.subsystems.cameras.DemonPhotonCamera;
 import frc.robot.subsystems.drive.SwerveDrive;
+import frc.robot.subsystems.TwistServo;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -93,7 +94,7 @@ public class RobotContainer {
   public RobotContainer() {
     this.configureSubsystemDefaultCommands();
     this.configureBindings();
-    this.configureAutonomous();
+    //this.configureAutonomous();
 
     kSwerveDrive.resetPose(new Pose2d(8.5, 4.0, Rotation2d.fromDegrees(0)));
     SmartDashboard.putData(kField);
@@ -129,46 +130,49 @@ public class RobotContainer {
           : 0;
       kSwerveDrive.resetPose(new Pose2d(currentPose.getX(), currentPose.getY(), Rotation2d.fromDegrees(awayAngle)));
     }, kSwerveDrive));
+    kDriverController.a().onTrue(new InstantCommand(() -> kCageTwist.latch(), kCageTwist));
+    kDriverController.b().onTrue(new InstantCommand(() -> kCageTwist.latch(), kCageTwist));
+
   }
 
   /** https://pathplanner.dev/home.html */
-  private void configureAutonomous() {
-    configureNamedCommandsForAuto();
+  // private void configureAutonomous() {
+  //   configureNamedCommandsForAuto();
 
-    // Build an auto chooser. This will use Commands.none() as the default option.
-    // https://pathplanner.dev/pplib-build-an-auto.html#create-a-sendablechooser-with-all-autos-in-project
-    m_autoChooser = AutoBuilder.buildAutoChooser();
+  //   // Build an auto chooser. This will use Commands.none() as the default option.
+  //   // https://pathplanner.dev/pplib-build-an-auto.html#create-a-sendablechooser-with-all-autos-in-project
+  //   m_autoChooser = AutoBuilder.buildAutoChooser();
 
-    // Another option that allows you to specify the default auto by its name
-    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+  //   // Another option that allows you to specify the default auto by its name
+  //   // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
 
-    SmartDashboard.putData("Auto Chooser", m_autoChooser);
-    this.configurePathPlannerLogging();
+  //   SmartDashboard.putData("Auto Chooser", m_autoChooser);
+  //   this.configurePathPlannerLogging();
+  // }
+
+  // /** https://pathplanner.dev/pplib-named-commands.html */
+  // private void configureNamedCommandsForAuto() {
+  //   NamedCommands.registerCommand("StopSwerve", new InstantCommand(() -> kSwerveDrive.stop(), kSwerveDrive));
+  // }
+
+  // /** https://pathplanner.dev/pplib-custom-logging.html */
+  // private void configurePathPlannerLogging() {
+  //   // Logging callback for current robot pose
+  //   PathPlannerLogging.setLogCurrentPoseCallback((pose) -> {
+  //     // Do whatever you want with the pose here
+  //     kField.setRobotPose(pose);
+  //   });
+
+  //   // Logging callback for target robot pose
+  //   PathPlannerLogging.setLogTargetPoseCallback((pose) -> {
+  //     // Do whatever you want with the pose here
+  //     kField.getObject("target pose").setPose(pose);
+  //   });
+
+  //   // Logging callback for the active path, this is sent as a list of poses
+  //   PathPlannerLogging.setLogActivePathCallback((poses) -> {
+  //     // Do whatever you want with the poses here
+  //     kField.getObject("path").setPoses(poses);
+  //   });
   }
-
-  /** https://pathplanner.dev/pplib-named-commands.html */
-  private void configureNamedCommandsForAuto() {
-    NamedCommands.registerCommand("StopSwerve", new InstantCommand(() -> kSwerveDrive.stop(), kSwerveDrive));
-  }
-
-  /** https://pathplanner.dev/pplib-custom-logging.html */
-  private void configurePathPlannerLogging() {
-    // Logging callback for current robot pose
-    PathPlannerLogging.setLogCurrentPoseCallback((pose) -> {
-      // Do whatever you want with the pose here
-      kField.setRobotPose(pose);
-    });
-
-    // Logging callback for target robot pose
-    PathPlannerLogging.setLogTargetPoseCallback((pose) -> {
-      // Do whatever you want with the pose here
-      kField.getObject("target pose").setPose(pose);
-    });
-
-    // Logging callback for the active path, this is sent as a list of poses
-    PathPlannerLogging.setLogActivePathCallback((poses) -> {
-      // Do whatever you want with the poses here
-      kField.getObject("path").setPoses(poses);
-    });
-  }
-}
+//}
