5012cdf8e584ae0b31caf7232110d3b6ebf866d7
Andrea-dot-sketch <148909097+Andrea-dot-sketch@users.noreply.github.com>
2025-01-29 21:35:55 +0000

    Servo Added

---
+++ src/main/java/frc/robot/subsystems/TwistServo.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Testing servo */
+public class TwistServo extends SubsystemBase {
+  Servo twist = new Servo(0);
+
+  public void latch(){
+    twist.setAngle(90);
+
+  }
+
+  public void unlatch(){
+    twist.setAngle(180);
+
+  }
+  
+}
