84af7d4ef4f646836105909a9c3477c7f8be49ca
Andrea-dot-sketch <pizzawithnothing@gmail.com>
2025-01-29 15:31:13 -0500

    prepareToScore added

---
+++ src/main/java/frc/robot/commands/PrepareToScore.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Coraller.CorallerPosition;
+import frc.robot.subsystems.Elevator.ElevatorPosition;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class PrepareToScore extends Command {
+  public enum Level {
+    STOW(ElevatorPosition.kStow, CorallerPosition.kStow),
+    L1(ElevatorPosition.kLevel1, CorallerPosition.kLevel1),
+    L2(ElevatorPosition.kLevel2, CorallerPosition.kLevel2),
+    L3(ElevatorPosition.kLevel3, CorallerPosition.kLevel3),
+    L4(ElevatorPosition.kLevel4, CorallerPosition.kLevel4);
+
+    private final ElevatorPosition elevatorPosition;
+    private final CorallerPosition corallerPosition;
+
+    private Level(ElevatorPosition elevatorPosition, CorallerPosition corallerPosition) {
+      this.elevatorPosition = elevatorPosition;
+      this.corallerPosition = corallerPosition;
+    }
+  }
+
+  private final Level level;
+
+  public PrepareToScore(Level level) {
+    addRequirements(RobotContainer.kElevator, RobotContainer.kCoraller);
+    this.level = level;
+  }
+
+  @Override
+  public void initialize() {
+    RobotContainer.kCoraller.setPosition(level.corallerPosition);
+    RobotContainer.kElevator.setPosition(level.elevatorPosition);
+  }
+}
