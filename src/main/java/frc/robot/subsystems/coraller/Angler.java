98879641009060afbba3df87a0fa7862e366e7ad
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2025-01-30 06:36:31 -0500

    reordering deps

--- src/main/java/frc/robot/subsystems/coraller/Angler.java
+++ src/main/java/frc/robot/subsystems/coraller/Angler.java
@@ -8,8 +8,8 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
