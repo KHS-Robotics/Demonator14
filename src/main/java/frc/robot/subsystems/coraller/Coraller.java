3b9876aaafd5b5054fd7faa08281e27f6a8d9e10
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2025-01-29 22:21:13 -0500

    reordering

--- src/main/java/frc/robot/subsystems/coraller/Coraller.java
+++ src/main/java/frc/robot/subsystems/coraller/Coraller.java
@@ -6,23 +6,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CorallerConfig;
 
 public class Coraller extends SubsystemBase {
-  public enum Configuration {
-    STOW(CorallerConfig.STOW_HEIGHT, CorallerConfig.STOW_ANGLE),
-    L1(CorallerConfig.L1_HEIGHT, CorallerConfig.L1_ANGLE),
-    L2(CorallerConfig.L2_HEIGHT, CorallerConfig.L2_ANGLE),
-    L3(CorallerConfig.L3_HEIGHT, CorallerConfig.L3_ANGLE),
-    L4(CorallerConfig.L4_HEIGHT, CorallerConfig.L4_ANGLE),
-    RECEIVE(CorallerConfig.RECEIVE_HEIGHT, CorallerConfig.RECEIVE_ANGLE);
-
-    private final double elevatorPosition;
-    private final double anglerPosition;
-
-    private Configuration(double elevatorPosition, double anglerPosition) {
-      this.elevatorPosition = elevatorPosition;
-      this.anglerPosition = anglerPosition;
-    }
-  }
-
   private final Elevator elevator;
   private final Angler angler;
   private final Intake intake;
@@ -51,5 +34,22 @@ public class Coraller extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public enum Configuration {
+    STOW(CorallerConfig.STOW_HEIGHT, CorallerConfig.STOW_ANGLE),
+    L1(CorallerConfig.L1_HEIGHT, CorallerConfig.L1_ANGLE),
+    L2(CorallerConfig.L2_HEIGHT, CorallerConfig.L2_ANGLE),
+    L3(CorallerConfig.L3_HEIGHT, CorallerConfig.L3_ANGLE),
+    L4(CorallerConfig.L4_HEIGHT, CorallerConfig.L4_ANGLE),
+    RECEIVE(CorallerConfig.RECEIVE_HEIGHT, CorallerConfig.RECEIVE_ANGLE);
+
+    private final double elevatorPosition;
+    private final double anglerPosition;
+
+    private Configuration(double elevatorPosition, double anglerPosition) {
+      this.elevatorPosition = elevatorPosition;
+      this.anglerPosition = anglerPosition;
+    }
   }
 }
