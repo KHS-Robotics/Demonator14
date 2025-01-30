a736d2f53832646991648ac58478bec52289b8cc
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2025-01-29 22:44:58 -0500

    whoops

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
-import frc.robot.subsystems.coraller.Coraller;
 
 public class Climber extends SubsystemBase {
   public enum AnchorPosition {
@@ -56,7 +55,6 @@ public class Climber extends SubsystemBase {
   }
 
   public void reelOut() {
-
     reel.setVoltage(-6);
   }
 
