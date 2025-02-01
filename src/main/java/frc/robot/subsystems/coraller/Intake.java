65b83208dbfddd49a96774c6a4ca30dc0d995f7c
LukeIsAlsoHere <lukewinn620@gmail.com>
2025-02-01 15:54:46 -0500

    Add sendable (#15)
    * added sendables
    
    * added drip
    
    * sure!
    
    Co-authored-by: Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
    Signed-off-by: LukeIsAlsoHere <lukewinn620@gmail.com>
    
    * is motor on added
    
    * fixing merge
    
    ---------
    
    Signed-off-by: LukeIsAlsoHere <lukewinn620@gmail.com>
    Signed-off-by: Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
    Co-authored-by: Chris Hoffman <99742+chrishoffman@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/coraller/Intake.java
+++ src/main/java/frc/robot/subsystems/coraller/Intake.java
@@ -7,9 +7,12 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-class Intake {
+class Intake extends SubsystemBase{
   private final SparkMax motor;
   private final SparkLimitSwitch sensor;
 
@@ -23,6 +26,18 @@ class Intake {
       SparkBase.PersistMode.kPersistParameters);
 
     sensor = motor.getForwardLimitSwitch();
+
+    SmartDashboard.putData(this);
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+    builder.setSmartDashboardType(getName());
+    builder.setSafeState(this::stop);
+    builder.setActuator(true);
+    builder.addBooleanProperty("hasCoral", this::hasCoral, null);
+    builder.addBooleanProperty("MotorIsOn", this::isMotorOn, null);
   }
 
   // TODO() find out volts and which are inversed
@@ -36,6 +51,11 @@ class Intake {
   
   public void stop() {
     motor.setVoltage(0);
+  }
+
+  //false if motor is off
+  public boolean isMotorOn(){
+    return !(motor.get() == 0);
   }
 
   public boolean hasCoral() {
