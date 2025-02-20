e5129e5c6e57bf32dc2571e3b1888a47f795fb14
Julietay17 <206124@bluedemons.org>
2025-02-20 20:49:31 +0000

    Changed the heights and angles to match the calculations that we did.

--- src/main/java/frc/robot/subsystems/coraller/CorallerSetpoints.java
+++ src/main/java/frc/robot/subsystems/coraller/CorallerSetpoints.java
@@ -7,12 +7,12 @@ package frc.robot.subsystems.coraller;
 final class CorallerSetpoints {
   protected final class AnglerSetpoints {
     public static final double STOW_ANGLE = 0;
-    public static final double L1_ANGLE = 0;
-    public static final double L2_ANGLE = 0;
+    public static final double L1_ANGLE = -40;
+    public static final double L2_ANGLE = -35;
     public static final double L2_ALGAE_ANGLE = 0;
-    public static final double L3_ANGLE = 0;
+    public static final double L3_ANGLE = -35;
     public static final double L3_ALGAE_ANGLE = 0;
-    public static final double L4_ANGLE = 0;
+    public static final double L4_ANGLE = -90;
     public static final double RECEIVE_ANGLE = 0;
   }
 
@@ -21,12 +21,12 @@ final class CorallerSetpoints {
     public static final double STOW_HEIGHT = 35;
     // ^2.057 Tonys
     public static final double RECEIVE_HEIGHT = 0;
-    public static final double L1_HEIGHT = 35;
-    public static final double L2_HEIGHT = 35;
+    public static final double L1_HEIGHT = 38;
+    public static final double L2_HEIGHT = 43;
     public static final double L2_ALGAE_HEIGHT = 35;
-    public static final double L3_HEIGHT = 50;
+    public static final double L3_HEIGHT = 58;
     public static final double L3_ALGAE_HEIGHT = 48;
-    public static final double L4_HEIGHT = 75;
+    public static final double L4_HEIGHT = 80;
   }
 
   protected final class FlickerSetpoints {
