106f5c3886b8aae3c8bc602d53eb5af46f55c38d
Andrea-dot-sketch <pizzawithnothing@gmail.com>
2025-02-27 18:49:56 -0500

    setpoint after slight mechanical adjustment

--- src/main/java/frc/robot/subsystems/coraller/CorallerSetpoints.java
+++ src/main/java/frc/robot/subsystems/coraller/CorallerSetpoints.java
@@ -12,7 +12,7 @@ final class CorallerSetpoints {
     public static final double L2_ALGAE_ANGLE = -65;
     public static final double L3_ANGLE = -35;
     public static final double L3_ALGAE_ANGLE = -65;
-    public static final double L4_ANGLE = -35;
+    public static final double L4_ANGLE = -50;
     public static final double RECEIVE_ANGLE = 35;
   }
 
