4be88dd53dcd4ded6d85baf4fed7b37d35203349
Andrea-dot-sketch <pizzawithnothing@gmail.com>
2025-03-24 17:38:45 -0400

    unused import

--- src/main/java/frc/robot/subsystems/climber/Anchor.java
+++ src/main/java/frc/robot/subsystems/climber/Anchor.java
@@ -15,7 +15,6 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.climber.ClimberConfig.AnchorConfig;
